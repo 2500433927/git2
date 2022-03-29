a0e34de9941ca422f0cafbdecabb96eb826fe918
2500433927 <102526676+2500433927@users.noreply.github.com>
2022-03-29 20:10:31 +0800

    pull commit

--- src/main/java/com/example/demo/test/test1.java
+++ src/main/java/com/example/demo/test/test1.java
@@ -8,5 +8,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("push");
+        System.out.println("pull");
+          
     }
 }
